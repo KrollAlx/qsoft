4f1c410ae1e22ff1b51d2475f8d837d0c9ccf9dd
KrollAlx <Aleksiy2000@yandex.ru>
2023-01-23 22:34:58 +0300

    ping middleware

--- main.go
+++ main.go
@@ -5,26 +5,37 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
+func PingMiddleware(c *gin.Context) {
+	if strings.ToLower(c.GetHeader("X-PING")) == "ping" {
+		c.Header("X-PONG", "pong")
+	}
+	c.Next()
+}
+
+func Handler(c *gin.Context) {
+	year, err := strconv.Atoi(c.Param("year"))
+	target := time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if year < time.Now().Year() {
+		daysGone := int64(time.Since(target).Hours()) / 24
+		c.String(http.StatusOK, "Days gone: %d", daysGone)
+	} else {
+		daysLeft := int64(time.Until(target).Hours()) / 24
+		c.String(http.StatusOK, "Days left: %d", daysLeft)
+	}
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/where/:year", func(c *gin.Context) {
-		year, err := strconv.Atoi(c.Param("year"))
-		target := time.Date(year, time.January,1,0,0,0,0, time.UTC)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if year < time.Now().Year() {
-			daysGone := int64(time.Since(target).Hours()) / 24
-			c.String(http.StatusOK, "Days gone: %d", daysGone)
-		} else {
-			daysLeft := int64(time.Until(target).Hours()) / 24
-			c.String(http.StatusOK, "Days left: %d", daysLeft)
-		}
-	})
+	router.Use(PingMiddleware)
+	router.GET("/where/:year", Handler)
 	err := router.Run(":3000")
 	if err != nil {
 		log.Println(err)
